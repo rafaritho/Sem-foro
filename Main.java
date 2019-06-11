cdcd80a0378197d9fc570a16a74584f4d341d7fc
Rafael Marques <50638004+rafaritho@users.noreply.github.com>
2019-06-10 21:54:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,119 @@
+import java.util.concurrent.*; 
+  
+//Classe compartilhada 
+class Shared  
+{ 
+    static int count = 0; 
+} 
+  
+class MyThread extends Thread 
+{ 
+    Semaphore sem; 
+    String threadName; 
+    public MyThread(Semaphore sem, String threadName)  
+    { 
+        super(threadName); 
+        this.sem = sem; 
+        this.threadName = threadName; 
+    } 
+  
+    @Override
+    public void run() { 
+          
+        // Inicia thread A 
+        if(this.getName().equals("A")) 
+        { 
+            System.out.println("Iniciando thread " + threadName); 
+            try 
+            { 
+                //Pede a permissão primeiro  
+                System.out.println(threadName + " está aguardando permissão"); 
+              
+                // Obtem a lock 
+                sem.acquire(); 
+              
+                System.out.println(threadName + " recebe permissão"); 
+          
+                // Acessa o a classe compartilhada
+                // a thread B aguarda até que essa 
+                // libere a lock 
+                for(int i=0; i < 5; i++) 
+                { 
+                    Shared.count++; 
+                    System.out.println(threadName + ": " + Shared.count); 
+          
+                    //Agora se mantem em espera para que 
+                    // a thread B execute 
+                    Thread.sleep(10); 
+                } 
+            } catch (InterruptedException exc) { 
+                    System.out.println(exc); 
+                } 
+          
+                // Thread A larga a permissão 
+                System.out.println(threadName + " libera a permissão"); 
+                sem.release(); 
+        } 
+          
+        // run by thread B 
+        else
+        { 
+            System.out.println("Iniciando thread " + threadName); 
+            try 
+            { 
+                // First, get a permit. 
+                System.out.println(threadName + " está aguardando permissão"); 
+              
+                // acquiring the lock 
+                sem.acquire(); 
+              
+                System.out.println(threadName + " recebe permissão"); 
+          
+                // Now, accessing the shared resource. 
+                // other waiting threads will wait, until this  
+                // thread release the lock 
+                for(int i=0; i < 5; i++) 
+                { 
+                    Shared.count--; 
+                    System.out.println(threadName + ": " + Shared.count); 
+          
+                    // Now, allowing a context switch -- if possible. 
+                    // for thread A to execute 
+                    Thread.sleep(10); 
+                } 
+            } catch (InterruptedException exc) { 
+                    System.out.println(exc); 
+                } 
+                // Release the permit. 
+                System.out.println(threadName + " libera a permissão"); 
+                sem.release(); 
+        } 
+    } 
+} 
+public class Main  
+{ 
+    public static void main(String args[]) throws InterruptedException  
+    { 
+        // creating a Semaphore object 
+        // with number of permits 1 
+        Semaphore sem = new Semaphore(1); 
+          
+        // creating two threads with name A and B 
+        // Note that thread A will increment the count 
+        // and thread B will decrement the count 
+        MyThread mt1 = new MyThread(sem, "A"); 
+        MyThread mt2 = new MyThread(sem, "B"); 
+          
+        // stating threads A and B 
+        mt1.start(); 
+        mt2.start(); 
+          
+        // waiting for threads A and B  
+        mt1.join(); 
+        mt2.join(); 
+          
+        // count will always remain 0 after 
+        // both threads will complete their execution 
+        System.out.println("count: " + Shared.count); 
+    } 
+} 
